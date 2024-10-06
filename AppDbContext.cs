de520e104bb2227a9809b1ecf9be2ee3dd14db74
MD Ashikuzzaman Abir <abir@xeon.email>
2024-10-06 17:11:16 +0600

    database executed

---
+++ AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql("server=localhost;database=database;user=root;password=1234",
+            new MySqlServerVersion(new Version(8, 0, 21))); // Change version as per your MySQL version
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+
+namespace HelloWorld
+{
+  static class Program
+  {
+    static void Main(string[] args)
+    {
+            using (var context = new AppDbContext())
+            {
+                // Add a new product
+                var product = new Product { Name = "Sample Product", Price = 10.99M };
+                context.Products.Add(product);
+                context.SaveChanges();
+
+                // Query the product
+                var products = context.Products.ToList();
+                foreach (var p in products)
+                {
+                    Console.WriteLine($"Product: {p.Name}, Price: {p.Price}");
+                }
+            }
+
+        }
+  }
+}
+
